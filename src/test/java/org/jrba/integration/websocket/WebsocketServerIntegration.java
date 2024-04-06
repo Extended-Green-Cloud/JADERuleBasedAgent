91cb1ac7c27324c37d8ce7da1f378db3ca94d859
soffi49 <zosia.wrona49@gmail.com>
2024-04-06 18:28:55 +0200

    [JRBA] Remove unused test

--- src/test/java/org/jrba/integration/websocket/WebsocketServerIntegration.java
+++ src/test/java/org/jrba/integration/websocket/WebsocketServerIntegration.java
@@ -1,9 +1,5 @@
 package org.jrba.integration.websocket;
 
-import java.nio.ByteBuffer;
-
-import org.java_websocket.WebSocket;
-import org.java_websocket.framing.Framedata;
 import org.jrba.environment.websocket.GuiWebSocketServer;
 
 import lombok.Getter;
@@ -11,22 +7,7 @@ import lombok.Getter;
 @Getter
 public class WebsocketServerIntegration extends GuiWebSocketServer {
 
-	private ByteBuffer pingReceived;
-	private String messageReceived;
-
 	public WebsocketServerIntegration() {
 		super(9669);
-	}
-
-	@Override
-	public void onWebsocketPing(final WebSocket conn, final Framedata f) {
-		super.onWebsocketPing(conn, f);
-		pingReceived = f.getPayloadData();
-	}
-
-	@Override
-	public void onMessage(WebSocket conn, String message) {
-		super.onMessage(conn, message);
-		messageReceived = message;
 	}
 }
